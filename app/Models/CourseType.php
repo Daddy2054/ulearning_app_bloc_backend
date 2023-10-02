76a6d6faa0e00d2a450beb1068e093f50d1970ee
Jean <acalto2@gmail.com>
2023-10-02 19:40:15 +0200

    163. Backend and do migrations part 4

---
+++ app/Models/CourseType.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CourseType extends Model
+{
+    use HasFactory;
+}
+
+
+// create command
+// php artisan make:model CourseType
