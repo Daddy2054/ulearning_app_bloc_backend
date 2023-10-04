1cb1572f195115e91b6f687be77798a3055a585c
Jean <acalto2@gmail.com>
2023-10-04 19:13:28 +0200

    178. Backend and understand api section part 18

---
+++ app/Http/Controllers/Api/CourseController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+// create command
+// php artisan make:controller Api/CourseController
+class CourseController extends Controller
+{
+    //
+}
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\UserController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,5 +14,10 @@ use App\Http\Controllers\Api\UserController;
 |
 */
 
-Route::post('/login', [UserController::class, 'createUser']);
-//Route::post('/auth/login', [UserController::class, 'loginUser']);
+Route::group(['namespace' => 'Api'], function () {
+
+    Route::post('/login', [UserController::class, 'createUser']);
+    //Route::post('/auth/login', [UserController::class, 'loginUser']);
+
+
+});
