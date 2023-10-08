1af46bcc726bb86816a5e02129c40f88f1718bab
Jean <acalto2@gmail.com>
2023-10-08 19:07:16 +0200

    208. Backend and work on checkOut end point part 4

--- app/Http/Controllers/Api/PayController.php
+++ app/Http/Controllers/Api/PayController.php
@@ -4,18 +4,50 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Stripe\Webhook;
+use Stripe\Customer;
+use Stripe\Price;
+use Stripe\Stripe;
+use Stripe\Checkout\Session;
+use Stripe\Exception\UnexpectedValueException;
+use Stripe\Exception\SignatureVerificationException;
+use App\Models\Course;
 
 class PayController extends Controller
 {
     //
     public function checkout(Request $request)
     {
+        try {
+            //code...
+            $user = $request->user();
 
-        $user = $request->user();
+            $token = $user->token;
+            $course_id = $request->id;
 
-        return response()->json([
-            'msg' => $user
-        ]);
+            //
+            // Stripe api key
+            //
+
+            $course_result = Course::where('id', '=', $course_id)->first();
+
+            if (empty($course_result)) {
+                return response()->json([
+                    'code' => 400,
+                    'msg' => 'Course does not exist'
+                ], 400);
+            }
+            return response()->json([
+                'code' => 200,
+                'msg' => 'Course found',
+                'data' => ''
+            ], 200);
+        } catch (\Throwable $th) {
+            //throw $th;
+            return response()->json([
+                'error' => $th->getMessage()
+            ], 500);
+        }
     }
 }
 
